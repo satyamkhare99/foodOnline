5f64e916ff5a908dad0b41e017749f2a827a42af
satyam <satyamkhare05071999@gmail.com>
2024-05-27 20:32:51 +0530

    third commit

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,8 @@ DATABASES = {
         'HOST': config('DB_HOST') 
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
